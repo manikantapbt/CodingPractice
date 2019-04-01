63a0b68d7db9cf9470cc6b569ce7e6cbf35167b3
mani <manikantapbt@gmail.com>
2019-04-01 18:52:13 +0530

    SubTree

---
+++ trees/src/main/java/SubTree.java
@@ -0,0 +1,44 @@
+public class SubTree {
+    public static void main(String[] args) {
+        Nodesh newRoot = new Nodesh(80);
+        Nodesh root = new Nodesh(20);
+        newRoot.left = root;
+        newRoot.right = new Nodesh(50);
+        root.left = new Nodesh(8);
+        root.right = new Nodesh(22);
+        root.left.left = new Nodesh(5);
+        root.left.right = new Nodesh(3);
+
+        Nodesh root1 = new Nodesh(20);
+        root1.left = new Nodesh(8);
+        root1.right = new Nodesh(22);
+        root1.left.left = new Nodesh(5);
+        root1.left.right = new Nodesh(3);
+        System.out.println(subTree(newRoot, root1));
+    }
+
+    public static boolean subTree(Nodesh root, Nodesh root1) {
+        if (root == null)
+            return false;
+        if (root1 == null)
+            return true;
+        boolean areIdentical = identicalTrees(root, root1);
+        if (areIdentical)
+            return true;
+        else
+            return subTree(root.left, root1) || subTree(root.right, root1);
+
+    }
+
+    public static boolean identicalTrees(Nodesh nodeFromTree1, Nodesh nodeFromTree2) {
+        if (nodeFromTree1 == null && nodeFromTree2 == null)
+            return true;
+        if ((nodeFromTree1 == null && nodeFromTree2 != null) || (nodeFromTree1 != null && nodeFromTree2 == null)) {
+            return false;
+        }
+        if (nodeFromTree1.data == nodeFromTree2.data) {
+            return identicalTrees(nodeFromTree1.left, nodeFromTree2.left) && identicalTrees(nodeFromTree1.right, nodeFromTree2.right);
+        }
+        return false;
+    }
+}
