694c913430a63033389196f9a3d13b015267d11a
mani <manikantapbt@gmail.com>
2019-04-02 14:57:43 +0530

    LCA by path finding

--- trees/src/main/java/LowestCommonAncestor.java
+++ trees/src/main/java/LowestCommonAncestor.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class LowestCommonAncestor {
     String aFound = "aFound";
     String bFound = "bFound";
@@ -17,9 +20,12 @@ public class LowestCommonAncestor {
         System.out.println("LCA(2, 4): " + LCA(root, 2, 4));
         System.out.println("LCA(3, 7): " + LCA(root, 3, 7));
         System.out.println("LCA(4, 7): " + LCA(root, 4, 7));
+        LCAByPath(root,3,7);
     }
 
     public static int LCA(Node root, int a, int b) {
+        // This is practice recursion
+        // traverse the tree only once to find LCA
         if (root == null) return -1;
         int left = LCA(root.left, a, b);
         int right = LCA(root.right, a, b);
@@ -43,5 +49,36 @@ public class LowestCommonAncestor {
         if(right==-1)
             return left;
         return -1;
+    }
+    public static void LCAByPath(Node root,int a,int b){
+        ArrayList<Integer> pathForA =new ArrayList();
+        findPath(root,a,pathForA);
+        System.out.println("pathForA = " + pathForA);
+        ArrayList<Integer> pathForB =new ArrayList();
+        findPath(root,b,pathForB);
+        System.out.println("pathForB = " + pathForB);
+
+        int i=0;
+        for ( ; i <pathForA.size() && i<pathForB.size() ; i++) {
+            if(!(pathForA.get(i)==pathForB.get(i))){
+                break;
+            }
+        }
+        System.out.println("LCA("+a+","+b +"): " + pathForA.get(i-1));
+    }
+    public static boolean findPath(Node root,int a,ArrayList<Integer> al){
+        if(root==null) return false;
+        if(root.key==a) {
+            al.add(root.key);
+            return true;
+        }
+        al.add(root.key);
+        boolean left =findPath(root.left,a,al);
+        boolean right=findPath(root.right,a,al);
+        if(left||right){
+            return true;
+        }
+        al.remove(al.size()-1);
+        return false;
     }
 }
